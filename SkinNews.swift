470bde547c652c3b92afd0ace09274118816e50e
michelle1510 <141178978+michelle1510@users.noreply.github.com>
2024-02-16 22:40:51 -0500

    Update SkinNews.swift

--- SkinNews.swift
+++ SkinNews.swift
@@ -1,95 +1,104 @@
-// Settings.swift
+//
+//  SkinNews.swift
+//  ClearSkinProject
+//
+//  Created by Michelle Li on 2/15/24.
+//
 
 import SwiftUI
 
-struct SkinNews: View {
-  var body: some View {
-    NavigationView {
-      List {
-        Section
-        {
-          Text("Search topic")
-        }
-        Section
-        {
-          Text("New Products")
-        }
-        Section
-        { //Photo fillers
-          Rectangle()
-                .fill(Color.pink)
-                .frame(width: 100, height: 100, alignment: .center)
-                .overlay(
-                    Text("1")
-                        .font(.largeTitle)
-                )
-            Rectangle()
-                .fill(Color.red)
-                .frame(width: 100, height: 50, alignment: .center)
-                .position(x: 50, y: 0)
-        }
-        Section
-        {
-          Text("Medical News")
-        }
-        Section
-        {
-            //Photo fillers
-            Rectangle()
-                  .fill(Color.pink)
-                  .frame(width: 100, height: 100, alignment: .center)
-                  .overlay(
-                      Text("1")
-                          .font(.largeTitle)
-                  )
-              Rectangle()
-                  .fill(Color.red)
-                  .frame(width: 100, height: 50, alignment: .center)
-                  .position(x: 50, y: 0)
-        }
-        Section
-        {
-          Text("Collection")
-        }
-        Section
-        {
-          //Photo fillers
-            Rectangle()
-                  .fill(Color.pink)
-                  .frame(width: 100, height: 100, alignment: .center)
-                  .overlay(
-                      Text("1")
-                          .font(.largeTitle)
-                  )
-              Rectangle()
-                  .fill(Color.red)
-                  .frame(width: 100, height: 50, alignment: .center)
-                  .position(x: 50, y: 0)
-        }
-      }
-      .listStyle(.insetGrouped)
-      .navigationTitle("Skin News")
-        
-        List {
-            Section
-            {
-                Text("Hello")
+struct NewsView: View {
+    
+    @State private var stuff = SkinStuff.examples()
+    @State private var news = MedicalNews.examples()
+    @State private var collection = Collection.examples()
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVStack {
+                    HStack {
+                        NavigationLink("Search Topic") {}
+                            .padding(4)
+                            .font(.headline)
+                            .frame(maxWidth: 400, alignment: .center)
+                            .padding(2)
+                            .foregroundStyle(.gray)
+                            .border(.black)
+                            .frame(alignment: .trailing)
+                        Spacer()
+                    }
+                    .padding(15)
+                    
+                    
+                    HStack {
+                        Text("New Products")
+                            .bold()
+                            .font(.title2)
+                            .multilineTextAlignment(.leading)
+                            .padding([.leading])
+                        Spacer()
+                    }
+                    
+                    ScrollView(.horizontal) {
+                        LazyHStack {
+                            ForEach(stuff) {stuff in
+                                SkinStuffCardView(stuff: stuff)
+                            }
+                        }
+                    }
+                    .padding(10)
+                    .frame(height: 200)
+                    .navigationTitle("Skin News")
+                    
+                    HStack {
+                        Text("Medical News")
+                            .font(.title)
+                            .bold()
+                            .multilineTextAlignment(.leading)
+                            .padding([.leading])
+                        Spacer()
+                    }
+                    
+                    ScrollView(.horizontal) {
+                        LazyHStack {
+                            ForEach(news) {news in
+                                MedicalNewsCardView(news: news)
+                                    .padding([.bottom, .trailing], 10)
+                                    .padding([.leading, .leading], 20)
+                            }
+                        }
+                    }
+                    .padding(10)
+                    .frame(height: 250)
+                    
+                    HStack {
+                        Text("Collection")
+                            .bold()
+                            .font(.title2)
+                            .multilineTextAlignment(.leading)
+                            .padding([.leading])
+                        Spacer()
+                    }
+                    
+                    ScrollView(.horizontal) {
+                        LazyHStack {
+                            ForEach(collection) {collection in
+                                CollectionCardView(collection: collection)
+                            }
+                        }
+                    }
+                    .padding(10)
+                    .frame(height: 200)
+                }
             }
         }
-        .listStyle(.insetGrouped)
-        .navigationTitle("ok")
-        
     }
-      
-    }
-    
-    
-  }
+}
 
-
-struct SkinNews_Preview: PreviewProvider {
+struct NewsView_Previews: PreviewProvider {
     static var previews: some View {
-        SkinNews()
+        NewsView()
     }
 }
 
